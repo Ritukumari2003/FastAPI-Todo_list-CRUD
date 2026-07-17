b414c714f4f550e1be204758b871ac574d01ce2e
Ritukumari2003 <2503.ritu@gmail.com>
2026-07-17 15:32:03 +0530

    Complete CRUD Operation

--- main.py
+++ main.py
@@ -14,3 +14,35 @@ class Todo(BaseModel):
 def create_todo(todo: Todo):
     todos.append(todo)
     return {'message': 'ToDo Added', 'data': todo}
+
+@app.get('/todos')
+def get_todos():
+    return todos
+
+@app.get('/todos/{todo_id}')
+def get_todo(todo_id: int):
+    for todo in todos:
+        if todo_id == todo.id:
+            return todo
+    return {"error": 'Todo not found with the given id!!'}
+
+@app.put('/todos/{todo_id}')
+def update_todo(todo_id: int, updated_todo: Todo):
+    for index, todo in enumerate(todos):
+        todos[index] = updated_todo
+        return {
+            'message': 'Data Updated',
+            'data' : updated_todo
+        }
+    return {'error': 'Todo not found with given id!!'}
+                
+@app.delete('/todos/{todo_id}')
+def delete_todo(todo_id: int):
+    for index, todo in enumerate(todos):
+        if todo.id == todo_id:
+            todos.pop(index)
+            return {'message': 'Deleted Todo Successfully!!'}            
+    return {'error': 'Todo not found with given id!!'}
+
+
+
